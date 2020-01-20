ea25049e2af9fec7a992e047343fcb2dd8323afb
vash3d <vash.3d@gmail.com>
2020-01-20 01:15:50 +0100

    First commit

---
+++ apps/nethassmo/nethassmo.py
@@ -0,0 +1,319 @@
+import appdaemon.plugins.hass.hassapi as hass
+import requests
+import time
+from configparser import ConfigParser, ExtendedInterpolation
+import os
+import os.path
+import datetime
+ 
+class Nethassmo(hass.Hass):
+ 
+    def initialize(self):
+        # READING CONFIG FILE
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        self.file = self.path + "/nethassmo.ini"
+        self.cfg_file = self.path + "/nethassmo.cfg"
+        self.config = ConfigParser(delimiters=(':'))
+        self.config.read(self.cfg_file)
+        self.user = self.config['ACCESS']['user']
+        self.pswd = self.config['ACCESS']['pswd']
+        self.client = self.config['ACCESS']['client']
+        self.secret = self.config['ACCESS']['secret']
+        self.data_error = False
+        # CHECKING ACCESS DATA IN CONFIG FILE
+        if (self.user == ""):
+            self.error("Username not found.", level = "ERROR")
+            self.data_error = True
+        else:
+            self.log("Reading username... OK")
+        
+        if (self.pswd == ""):
+            self.error("Password not found.", level = "ERROR")
+            self.data_error = True
+        else:
+            self.log("Reading password... OK")
+
+        if (self.client == ""):
+            self.error("Client ID not found.", level = "ERROR")
+            self.data_error = True
+        else:
+            self.log("Reading client ID... OK")
+
+        if (self.secret == ""):
+            self.error("Client secret not found.", level = "ERROR")
+            self.data_error = True
+        else:
+            self.log("Reading client secret... OK")
+            
+        if self.data_error == True:
+            self.error("Please fill [ACCESS] section in configuration file with your data.", level = "ERROR")
+        else:
+            self.log("Netatmo Access Data: OK")
+            self.get_token()
+        
+
+        self.persons = self.args['persons']
+        if self.persons == []:
+            self.error("Add persons' entity to monitor in file apps.yaml", level = "ERROR")
+        else:
+            for person in self.persons:
+                self.listen_state(self.set_state, entity=person)
+        
+        guest_mode = self.args['guest_mode_switch']
+        if guest_mode != "input_boolean.not_set_yet":
+            self.listen_state(self.guestmode, guest_mode)
+
+
+        now = datetime.datetime.now()
+        delta = datetime.timedelta(seconds=30)
+        wait = now + delta
+        self.run_every(self.refresh_token, wait, 150*60)
+        
+    
+    #################################################
+    
+    #           get_token FUNCIONN 
+
+    #################################################
+
+    def get_token(self):
+        self.config = ConfigParser(delimiters=(':'))
+        self.config.read(self.cfg_file)
+        if not self.config.has_option('TOKEN', 'token'): # REQUSTING TOKEN IF NOT ALREADY PRESENT
+            username = self.config['ACCESS']['user']
+            pswd = self.config['ACCESS']['pswd']
+            client_id = self.config['ACCESS']['client']
+            client_secret = self.config['ACCESS']['secret']
+            payload = {'grant_type': 'password',
+                    'username': username,
+                    'password': pswd,
+                    'client_id': client_id,
+                    'client_secret': client_secret,
+                    'scope': 'read_camera write_camera access_camera'}
+            self.log("Requesting Token...")
+            try:
+                response = requests.post("https://api.netatmo.com/oauth2/token",
+                                        data=payload)
+                response.raise_for_status()
+                access_token = response.json()["access_token"]
+                refresh_token = response.json()["refresh_token"]            
+                # scope = response.json()["scope"]
+                # validity = response.json()["expires_in"]
+
+                self.log("Token acquired. Writing to configuration file...")
+                self.config['TOKEN'] = {}
+                self.config['TOKEN']['token'] = access_token
+                self.config['TOKEN']['refresh'] = refresh_token
+                
+                
+                with open(self.cfg_file, 'w') as configfile:
+                    self.config.write(configfile)
+
+            except requests.exceptions.HTTPError as error:
+                self.log(error.response.status_code, error.response.text)
+        else:
+            self.log("Access Token found.")
+
+        self.get_home_data()
+
+
+    #################################################
+    
+    #           get_home_data FUNCIONN 
+
+    #################################################
+
+    def get_home_data(self, *args):
+        self.config = ConfigParser(allow_no_value=True, delimiters=(':'))
+        self.config.read(self.cfg_file)
+        
+        if not self.config.has_section('HOME'): # REQUSTING HOME DATA IF NOT ALREADY PRESENT
+            self.log("Requesting Home data...")
+            token = self.config['TOKEN']['token']
+            params = {
+                'access_token': token,
+            }
+
+            try:
+                response = requests.post("https://api.netatmo.com/api/gethomedata?size=0",
+                                        params=params)
+                response.raise_for_status()
+                self.config['HOME'] = {}
+                self.config['PERSONS'] = {}
+                
+                home_id = response.json()["body"]['homes'][0]['id']
+                self.config['HOME']['home_id'] = home_id
+                # self.log("Home ID: {}".format(home_id))
+                persons = response.json()["body"]['homes'][0]['persons']
+                for person in persons:
+                    self.config['PERSONS'][person['pseudo']] = person['id']    
+                
+                with open(self.cfg_file, 'w') as configfile:
+                    self.config.write(configfile)
+                
+            except requests.exceptions.HTTPError as error:
+                self.log(error.response.status_code, error.response.text)
+        else:
+            self.log("Home data found.")
+
+        self.sensors = self.args['persons']
+        if self.sensors == []:
+            self.error("Add persons' entity to monitor in file apps.yaml", level = "ERROR")
+        else:
+            if not self.config.has_section('SENSORS'):
+                self.log("Adding person sensors to configuration file...")
+                self.config['SENSORS'] = {}
+                self.config.optionxform=str
+                self.config.set('SENSORS', '; Edit value (if needed) with the correspondig name from [PERSONS] section')
+                self.config.set('SENSORS', '; e.g. ${PERSONS:name}')
+                
+                for sensor in self.sensors:
+                    friendly_name = self.get_state(sensor, attribute='friendly_name')
+                    self.config['SENSORS']['sensor_' + friendly_name.lower()] = '${PERSONS:' + friendly_name.lower() + '}'
+
+                with open(self.cfg_file, 'w') as configfile:
+                        self.config.write(configfile)
+
+
+    #################################################
+    
+    #           refresh_token FUNCIONN 
+
+    #################################################
+
+    def refresh_token(self, *args):
+        self.config = ConfigParser()
+        self.config.read(self.cfg_file)
+
+        if self.config.has_option('TOKEN', 'refresh'):
+            self.log("Refreshing Token validity...")
+            client_id = self.config['ACCESS']['client']
+            client_secret = self.config['ACCESS']['secret']
+            token = self.config['TOKEN']['token']
+            refresh = self.config['TOKEN']['refresh']
+
+            payload = {'grant_type': 'refresh_token',
+                    'client_id': client_id,
+                    'client_secret': client_secret,
+                    'refresh_token': refresh}
+
+            try:
+                response = requests.post("https://api.netatmo.com/oauth2/token",
+                                        data=payload)
+                response.raise_for_status()
+                access_token = response.json()["access_token"]
+                refresh_token = response.json()["refresh_token"]
+                scope = response.json()["scope"]
+                if ("write_camera" and "access_camera") not in scope:
+                    self.error("Needed scopes are missing. Requesting new token and scope...", level = "WARNING")
+                    self.get_token()
+                else:
+                    if access_token == token:
+                        self.log("Token validity refreshed!")
+                    else:
+                        self.log("Token renewd!")
+                        self.config['TOKEN']['token'] = access_token
+
+                    with open(self.file, 'w') as configfile:
+                        self.config.write(configfile)
+
+            except requests.exceptions.HTTPError as error:
+                self.log(error.response.status_code, error.response.text)
+
+
+    def set_state(self, entity, attributes, old, new, kwargs):
+        self.config = ConfigParser(interpolation=ExtendedInterpolation())
+        self.config.read(self.cfg_file)
+        token = self.config['TOKEN']['token']
+        home = self.config['HOME']['home_id']
+
+        if (new == 'home' and old != new):
+            friendly_name = self.get_state(entity, attribute='friendly_name')
+            person_id = self.config['SENSORS']['sensor_' + friendly_name.lower()]
+
+            params = {
+                'access_token': token,
+                'home_id': home,
+                'person_ids': '[ "' + person_id + '" ]'
+            }
+            try:
+                response = requests.post("https://api.netatmo.com/api/setpersonshome",
+                                        params=params)
+                response.raise_for_status()
+                status = response.json()["status"]
+                self.log("Setting {} home...".format(friendly_name.capitalize()))
+                self.log("Response: {}".format(status.upper()))
+            except requests.exceptions.HTTPError as error:
+                self.log(error.response.status_code, error.response.text)
+
+        elif (new == 'not_home' and old == 'home'):
+            friendly_name = self.get_state(entity, attribute='friendly_name')
+            person_id = self.config['SENSORS']['sensor_' + friendly_name.lower()]
+            
+            params = {
+                'access_token': token,
+                'home_id': home,
+                'person_id': person_id
+            }
+            try:
+                response = requests.post("https://api.netatmo.com/api/setpersonsaway",
+                                        params=params)
+                response.raise_for_status()
+                status = response.json()["status"]
+                self.log("Setting {} away...".format(friendly_name.capitalize()))
+                self.log("Response: {}".format(status.upper()))
+            except requests.exceptions.HTTPError as error:
+                self.log(error.response.status_code, error.response.text)
+
+
+    #################################################
+    
+    #           guestmode FUNCIONN 
+
+    #################################################
+
+    def guestmode(self, entity, attributes, old, new, kwargs):
+        self.config = ConfigParser()
+        self.config.read(self.file)
+
+        token = self.config['TOKEN']['token']
+
+        params = {
+            'access_token': token
+        }
+
+        try:
+            response = requests.post("https://api.netatmo.com/api/gethomedata?size=0",
+                                    params=params)
+            response.raise_for_status()
+            cameras = response.json()["body"]['homes'][0]['cameras']
+            for camera in cameras:
+                if (new == 'off'):
+                    if (camera['status'] == 'off'):
+                        try:
+                            response = requests.post(camera['vpn_url'] + "/command/changestatus?status=on")
+                            response.raise_for_status()
+                            status = response.json()["status"]
+                            self.log("Turning ON monitoring...")
+                            self.log("Response: {}".format(status.upper()))
+                        except requests.exceptions.HTTPError as error:
+                            self.log(error.response.status_code, error.response.text)
+                    else:
+                        self.log("Camera is already ON")
+                elif (new == 'on'):
+                    if (camera['status'] == 'on'):
+                        try:
+                            response = requests.post(camera['vpn_url'] + "/command/changestatus?status=off")
+                            response.raise_for_status()
+                            status = response.json()["status"]
+                            self.log("Turning OFF monitoring...")
+                            self.log("Response: {}".format(status.upper()))
+                        except requests.exceptions.HTTPError as error:
+                            self.log(error.response.status_code, error.response.text)
+                    else:
+                        self.log("Camera is already OFF")
+        
+        except requests.exceptions.HTTPError as error:
+            self.log(error.response.status_code, error.response.text)
+
+ 
---
+++ readme.md
@@ -0,0 +1,126 @@
+# NetHassmo
+## Automate Welcome Camera presence and monitoring.
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)
+
+
+
+
+## Installation
+
+Using [HACS](https://github.com/custom-components/hacs), to install and track updates, is reccomended.
+
+Alternatively, you can download all the files inside the `apps` directory to your local `apps` directory, then add the configuration to enable the `nethassmo` module.
+
+## How it works
+
+This app listen to (user-defined) person entities (and consequently to the associated device tracker) and set Home or Away the corresponding person in Netatmo Welcome camera.
+This is especially usefull to avoid manually setting you out when you leave home.
+
+There's is also a "bonus" function to activate/deactivate the camera monitoring.
+It's called GuestMode cause it's particularly usefull when you have guests at home to avoid dozens of notification of unknown faces.
+
+## Configuration
+
+To use this app you need an Access Token from Netatmo.  
+Browse to https://dev.netatmo.com and login with your account or create one.
+Create a new app, give it a name and a description.  
+You have to fill `data protection officer name` and `data protection officer email` fields in order to move on (you can simply put your name and email here).  
+When you click on SAVE you obtain a `client id` and a `client secret` code. Keep this page open.
+
+In nethassmo folder open nethassmo.cfg with your favorite code editor and fill all the required fields.  
+```ini
+[ACCESS]
+user : youremail@email.com
+pswd : yourpassword
+client : 4f3ed70814549fb7f98b5t72
+secret : D8NzNVhdLMcy0QeTsin3tCcjmu0HGfZme9hKoEXkR
+```
+key | required | description
+-- | -- | --
+`user:`| true | the username of your netatmo dev account (email)
+`pswd:`| true | the password of your netatmo dev account
+`client:`| true | the client id from your app page
+`secret:`| true | the client secret from your app page
+Save the file.
+
+Next, you can add nethassmo configuration to your apps.yaml file.  
+Insert module and class as usual then you have to specify the person sensor the app should listen to. Optionally you can specify an entity (e.g. input_boolean) to control "Guest Mode".
+
+```yaml
+nethassmo:
+  module: nethassmo
+  class: Nethassmo
+  persons: ['person.john', 'person.jane', 'person.name_3']
+  guest_mode_switch: input_boolean.guestmode
+```
+
+key | required | type | default | description
+-- | -- | -- | -- | --
+`module` | True | string | | The module name of the app.
+`class` | True | string | | The name of the Class.
+`persons` | True | list | | The person entities to monitor.
+`guest_mode_switch` | False | string | | The entity_id you want to use to turn monitoring on/off. Usually an input boolean
+
+Save the file and restart Appdaemon if it not reload the apps.  
+The first time Nethassmo is executed it will use the credentials you have previously added for requesting to Netatmo an access token, and your home and persons data.  
+All the data will be then added to `nethassmo.cfg`
+
+```ini
+[ACCESS]
+user : youremail@email.com
+pswd : yourpassword
+client : 4f3ed70814549fb7f98b5t72
+secret : D8NzNVhdLMcy0QeTsin3tCcjmu0HGfZme9hKoEXkR
+
+[TOKEN]
+token : 6c7810f49ad10534d29b51e7|ff78959ebeee5fb611a4aba1a37588f5
+refresh : 6c7810f49ad10534d29b51e7|fb1d195a5d58cfe6e61287c3a3bfc619
+
+[HOME]
+home_id : 6c7810f49ad10534d29b51e7
+
+[PERSONS]
+john : not6424b-29c3-4820-79n4-ec9573b68h27
+jane : 7u3006cf-c756-4e7c-b44a-e3d7e98637f4
+name_3 : xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx
+
+[SENSORS]
+; Edit value (if needed) with the correspondig name from [PERSONS] section
+; e.g. ${PERSONS:name}
+sensor_john : ${PERSONS:john}
+sensor_jane : ${PERSONS:jane}
+sensor_name_3 : ${PERSONS:name_3}
+```
+<u> **IMPORTANT** </u>  
+Pay attention at [SENSORS] section.  
+The `key` is based on the friendly name of the monitored person.name from Home Assistant (the one you have specified in apps.yaml).  
+The corresponding value is a reference to the value of `[PERSONS]` section.
+By default Nethassmo app assumes that persons' names in Home Assistant and the ones from Netatmo Welcome camera are the same.  
+<u>**If that's not the case you have to manually change the value.**</u>
+
+Taking as an example the configuration above, if in Netatmo Welcome camera the user john had been configured as Johnny instead of John the resulting auto-configuration would had been
+```ini
+[PERSONS]
+johnny : not6424b-29c3-4820-79n4-ec9573b68h27
+jane : 7u3006cf-c756-4e7c-b44a-e3d7e98637f4
+name_3 : xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx
+
+[SENSORS]
+; Edit value (if needed) with the correspondig name from [PERSONS] section
+; e.g. ${PERSONS:name}
+sensor_john : ${PERSONS:john}
+sensor_jane : ${PERSONS:jane}
+sensor_name_3 : ${PERSONS:name_3}
+```
+So in this case you have to modify `${PERSONS:john}` with `${PERSONS:johnny}`
+
+## Issues/Feature Requests
+
+Please feel free to open any issues or feature requests!
+
+## Note
+Theoretically the app should work with multiple cameras but at the moment I can only test it with the one I have.  
+If you want you can help me to add a second one ;)
+
+<br><a href="https://www.buymeacoffee.com/aneisch" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-black.png" width="150px" height="35px" alt="Buy Me A Coffee" style="height: 35px !important;width: 150px !important;" ></a>
